13472decec73f98d78ef0ccc82e33ea11d4169d2
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-06 15:28:12 +0300

    Added boolean variables

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    boolean isJava = true;
+    boolean isFishTasty = false;
+    System.out.println("Is Java fun?" + isJava);
+    System.out.println("Is the fish tasty?" + isFishTasty);
+
   }
 }

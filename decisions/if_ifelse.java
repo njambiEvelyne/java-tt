762a959faebaef8ebf93c92819ea5eb8c840458c
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 02:36:42 +0300

    Added a new if_ifelse file with the code block for the execution

---
+++ decisions/if_ifelse.java
@@ -0,0 +1,21 @@
+package decisions;
+//import java.util.*;
+
+public class if_ifelse {
+  public static void main(String[] args) {
+    int y = 20;
+    if(y == 10){
+      System.out.println("y is 10");
+    }
+    else if(y ==15){
+      System.out.println("y is 15");
+    }
+    else if(y ==20){
+      System.out.println("y is 20");
+    }
+    else{
+      System.out.println("y is not represented");
+    }
+    
+  }
+}

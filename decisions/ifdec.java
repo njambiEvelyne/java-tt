7fffc6e595d1ac684e32580da0a981bab1de1eb5
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 02:06:54 +0300

    Added if for decision making

---
+++ decisions/ifdec.java
@@ -0,0 +1,11 @@
+package decisions;
+
+public class ifdec {
+  public static void main(String[] args) {
+    int i =0;
+    if (i < 15){
+      System.out.println("Condition is True");
+    }
+  }
+  
+}

ab1348d9ac563f873297cb565ce2e6f541db96c8
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 03:36:54 +0300

    Added tenary operators

---
+++ decisions/tenaryopp.java
@@ -0,0 +1,10 @@
+package decisions;
+
+public class tenaryopp {
+  public static void main(String[] args) {
+    int a=10, b = 20;
+    int max = (a > b)? a : b;
+    System.out.println("Maximum is " + max);
+
+  }
+}

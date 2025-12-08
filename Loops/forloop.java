49aa68a9977ec98b7ed18838c1da0077c63d9a47
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 06:50:59 +0300

    Added for-each statement to iterate over the array

--- Loops/forloop.java
+++ Loops/forloop.java
@@ -5,10 +5,17 @@ public class forloop {
     for (int i = 0; i<=10; i++){
       System.out.println(i);
     }
-    
+    //Use of the for-each to iterate over an array of items
     String [] names = {"Sweta", "Gudly", "Amiya"};
     for(String name: names){
       System.out.println("Name: " + name);
     }
+    //Using for to iterate over an array of items
+    String[] names2 = {"Evelyne", "John", "David"};
+
+  for (int i = 0; i < names2.length; i++) {
+      System.out.println("Name: " + names2[i]);
+  }
+  // You must manage the index (i) and boundary (names.length)
   }
 }

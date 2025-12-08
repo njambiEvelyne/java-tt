baefb9163f45386b4043bc9b21f1593618bab4f9
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 03:58:28 +0300

    Added for loop for name generation

--- Loops/forloop.java
+++ Loops/forloop.java
@@ -3,7 +3,12 @@ package Loops;
 public class forloop {
   public static void main(String[] args) {
     for (int i = 0; i<=10; i++){
-      System.out.print(i+ ", ");
+      System.out.println(i);
+    }
+    
+    String [] names = {"Sweta", "Gudly", "Amiya"};
+    for(String name: names){
+      System.out.println("Name: " + name);
     }
   }
 }

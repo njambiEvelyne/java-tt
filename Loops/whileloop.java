7ce85e49566c9634ac8f4775e46d6f92596ab0db
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 04:05:58 +0300

    Corrected the while loop to have infinite numer of execution

--- Loops/whileloop.java
+++ Loops/whileloop.java
@@ -2,6 +2,11 @@ package Loops;
 
 public class whileloop {
   public static void main(String[] args) {
-    
+    int s = 0;
+    while (s <=10) {
+      System.out.println(s);
+      s++;
+      
+    }
   }
 }

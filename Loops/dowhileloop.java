d93d5f0630e0ba8aa5b0566ec009e7ed99722ab9
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 06:30:06 +0300

    Added a series of nested for loops

--- Loops/dowhileloop.java
+++ Loops/dowhileloop.java
@@ -7,5 +7,21 @@ public class dowhileloop {
       System.out.print(r + ", ");
       r++;
     }while(r <=10);
+
+int x = 5;
+if (x > 2){
+    if (x > 4){
+        System.out.println("A");
+    }
+    else{
+        System.out.println("B");
+    }
+  }
+
+for (int i = 1; i <= 3; i++) {
+  for (int j = 1; j <= 2; j++) {
+      System.out.print(i + "," + j + " ");
+    }
+}
   }
 }

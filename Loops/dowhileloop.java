59df6fb2a1270aefc8b52315a9808a7e9faaaef3
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-08 04:14:12 +0300

    Added do-while loops

---
+++ Loops/dowhileloop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class dowhileloop {
+  public static void main(String[] args) {
+    int r = 0;
+    do{
+      System.out.print(r + ", ");
+      r++;
+    }while(r <=10);
+  }
+}

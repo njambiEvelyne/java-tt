f8374ca2c3b7b252c8c681aef6ec8b0a93775d54
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-09 06:11:14 +0300

    Added an instance method

---
+++ Methods/callstack.java
@@ -0,0 +1,30 @@
+package Methods;
+
+public class callstack {
+      public static void D(){
+      float d = 40.5f;
+      System.out.println("In method D");
+    }
+
+    public static void C(){
+      double c = 30.5;
+      System.out.println("In method C");
+    }
+
+    public static void B(){
+      int b = 20;
+      C();
+      System.out.println("In method B");
+    }
+
+    public static void A (){
+      int a = 10;
+      B();
+      System.out.println("In method A");
+    }
+    public static void main(String[] args) {
+      A();
+      D();
+    }
+  }
+

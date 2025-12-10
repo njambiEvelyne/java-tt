5c2380e1744ef8ef9d984cbc1b84eb3a272e9af2
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-10 16:28:15 +0300

    Added instance methods

---
+++ Methods/staticandinst.java
@@ -0,0 +1,13 @@
+package Methods;
+
+class staticandinst {
+  public static void greet(){
+    System.out.println("Hello Evelyne!");
+  }
+
+  public static void main(String[] args) {
+    //Calling the method directly
+    greet();
+    
+  }
+}

f14d6f2d81c5649a315edabd521edd7c148cc22c
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-10 05:57:10 +0300

    Added a method for name display

---
+++ Methods/methods.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class methods {
+  public static void main(String[] args) {
+    String name = "Evelyne";
+    hello(name);
+  }
+  static void hello(String name){
+    System.out.println("Hello " + name);
+  }
+}

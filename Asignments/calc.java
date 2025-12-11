c51ddc38b3aebd265789b7a2ae4ef5ee9312246d
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-11 07:28:16 +0300

    Implemented acceptance for user input

--- Asignments/calc.java
+++ Asignments/calc.java
@@ -6,6 +6,15 @@ public class calc {
 
     System.out.println("What is your name?");
     String name = scanner.nextLine();
-    System.out.println("Hello " +name);
+    System.out.println("Hello " +name+ " Welcome to the calculator program");
+
+    
+    System.out.println("Enter the first digit: ");
+    int num1 = scanner.nextInt();
+    
+    System.out.println("Enter the second number: ");
+    int num2 = scanner.nextInt();
+    
+
   }
 }

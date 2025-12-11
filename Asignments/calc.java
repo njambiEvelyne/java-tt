d8579fce82ccb6a94912a8b6a4a56fd48d199ae8
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-11 07:19:09 +0300

    Added a calculator file

---
+++ Asignments/calc.java
@@ -0,0 +1,11 @@
+package Asignments;
+import java.util.Scanner;
+public class calc {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What is your name?");
+    String name = scanner.nextLine();
+    System.out.println("Hello " +name);
+  }
+}
--- Methods/staticandinst.java
+++ Methods/staticandinst.java
@@ -1,9 +1,18 @@
 package Methods;
 
+//1. Static methods
 class staticandinst {
   public static void greet(){
     System.out.println("Hello Evelyne!");
   }
+
+//2. Instance methods
+Class Test{
+  //instance method
+  public void test(String n){
+    this.n = n;
+  }
+}
 
   public static void main(String[] args) {
     //Calling the method directly
@@ -11,6 +20,9 @@ class staticandinst {
 
     //Using the class name
     staticandinst.greet();
+    //create an instance of the class
+    Test t = new Test();
     
+    t.test("Geeks for Geeks")
   }
 }

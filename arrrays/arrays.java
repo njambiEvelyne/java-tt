f4b1d40826c245256f0a7efa250186034344e885
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-13 07:05:18 +0300

    Added an array file

---
+++ arrrays/arrays.java
@@ -0,0 +1,15 @@
+package arrrays;
+class Student{
+  public int roll_no;
+  public String name;
+
+  Student(int roll_no, String name){
+    this.roll_no = roll_no;
+    this.name = name;
+  }
+}
+public class arrays {
+  public static void main(String[] args) {
+    
+  }
+}

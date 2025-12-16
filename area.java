57dfab785fa836e475f127204d332d0163cac2ce
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-16 07:40:00 +0300

    Added a new class implementing the shape class

--- area.java
+++ area.java
@@ -37,16 +37,36 @@ class Circle implements Shape {
         return 2 * Math.PI * r;
     }
 }
+class Rectangle implements Shape{
+    double a, b, c, d;
+    Rectangle(double a, double b, double c ,double d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    }
+    public double area (){
+        return a*b;
+    }
+    public double perimeter(){
+        return a+b+c+d;
+    }
+
+    }
 
 public class area{
     public static void main(String[] args) {
         Shape t = new Triangle(3, 4, 5);
         Shape c = new Circle(7);
+        Shape r = new Rectangle(12, 10, 012, 10);
 
         System.out.println("Triangle Area: " + t.area());
         System.out.println("Triangle Perimeter: " + t.perimeter());
-
+       
         System.out.println("Circle Area: " + c.area());
         System.out.println("Circle Perimeter: " + c.perimeter());
+
+        System.out.println("Rectangle Area: " + r.area());
+        System.out.println("Rectangle Perimeter: " + r.perimeter());
     }
 }

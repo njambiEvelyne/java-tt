4b7a57a310194039e89bf825cdbab74f233be4f3
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-18 07:22:40 +0300

    Deleted a file

---
+++ arrrays/arrclass.java
@@ -0,0 +1,13 @@
+// package arrrays;
+// import java.lang.reflect.Array;
+// import java.util.Arrays;
+
+// public class arrclass {
+//   int arr [] = {10,20,30,22};
+//   //Array.sort(arr);
+//   int intkey =  22;
+
+//   System.out.println(
+//     intkey + "found at index = " + Arrays.binarySearch(arr, intkey));
+  
+// }

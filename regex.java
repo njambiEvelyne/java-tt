82b7b440f3c48a6774a238965133c4249d092875
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-21 16:58:06 +0300

    Added regex lines to check input

---
+++ regex.java
@@ -0,0 +1,8 @@
+import java.util.regex.Pattern;;
+public class regex {
+  public static void main(String[] args) {
+    System.out.println(Pattern.matches("geeks.*", "geeksforgeeks"));//true
+
+    System.out.println(Pattern.matches("geeks[0-9+", "geeks12s"));//false
+  }
+}

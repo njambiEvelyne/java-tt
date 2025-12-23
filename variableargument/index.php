c35d15c39c07084c2f3de400daf24c58c8ff981a
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-23 23:49:10 +0300

    Modified the files

--- variableargument/index.php
+++ variableargument/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
 <?php
 /*
 ===========================================
@@ -260,3 +270,6 @@ echo "</ul>\n";
 
 echo "<p><strong>Happy coding with PHP!</strong></p>\n";
 ?>
+
+</body>
+</html>

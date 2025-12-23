d760e403e81e0a9ebeaf924065e12aeac23df1b3
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-23 07:15:37 +0300

    Added pattern matchers

--- regex.java
+++ regex.java
@@ -1,8 +1,18 @@
-import java.util.regex.Pattern;;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class regex {
   public static void main(String[] args) {
     System.out.println(Pattern.matches("geeks.*", "geeksforgeeks"));//true
 
     System.out.println(Pattern.matches("geeks[0-9]+", "geeks12s"));//false
+
+    Pattern p = Pattern.compile("geeks");
+    Matcher m = p.matcher("geeksforgeeks");
+
+    while (m.find()) {
+      System.out.println("Pattern found from " + m.start() + "to " + (m.end()-1));
+      
+    }
   }
 }

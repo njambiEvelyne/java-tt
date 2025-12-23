da513b66688a81d1cba814a12d97093a3ba2d40f
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-24 00:08:37 +0300

    Added boooean statements for comparison of actions

--- phpfolder/index2.php
+++ phpfolder/index2.php
@@ -1,4 +1,21 @@
 <?php
   $name = "Evelyne";
   echo "My name is " . $name . ". I am 20 years old";
+
+  $action = "Show_version";
+  $show_seperators = true;
+
+  if($action == "Show_version"){
+    echo "The version is 1.23";
+  }
+
+  if($show_seperators == TRUE){
+    echo "<hr>\n";
+  }
+
+  if($show_seperators){
+    echo "<hr>\n";
+  }
+
+
 ?>

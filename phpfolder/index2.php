3596ba7397f51dede5b704d5b44db29adf10db62
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-24 05:12:36 +0300

    Added escape sequences

--- phpfolder/index2.php
+++ phpfolder/index2.php
@@ -1,38 +1,44 @@
 <?php
   $name = "Evelyne";
-  echo "My name is " . $name . ". I am 20 years old";
+  echo "My name is " . $name . ". I am 20 years old\n";
 
   $action = "Show_version";
   $show_seperators = true;
 
   if($action == "Show_version"){
-    echo "The version is 1.23";
+    echo "The version is 1.23\n";
   }
 
   if($show_seperators == TRUE){
-    echo "<hr>\n";
+    echo "\n";
   }
 
   if($show_seperators){
     echo "<hr>\n";
   }
+
 $num1 = 20;
 $num2 = 30;
 $sum = $num1 + $num2;
-echo $num1 . " + " . $num2 . " = " . $sum;
-echo " ";
+echo $num1 . " + " . $num2 . " = " . $sum . "\n";
 
 $div = $num1 / $num2;
-echo ($num1 . "/" . $num2 . " = " . $div);
+echo $num1 . "/" . $num2 . " = " . $div . "\n";
 
+echo "Array 1 (print_r)\n";
+echo "<pre>";
 $array1 = array(
   "name" => "Evelyne",
   "age" => 22,
   "address" => "evelyne nganga76@gmail.com"
 );
 print_r($array1);
-echo "Name: " .$array1["name"]. " Age: ". $array1["age"]. " Address: ".$array1["address"];
+echo "</pre>";
 
+echo "Name: " .$array1["name"]. " | Age: ". $array1["age"]. " | Address: ".$array1["address"] . "\n";
+
+echo "<h3>Array 2 (var_dump):</h3>\n";
+echo "<pre>";
 $array2 = array(
     1    => "a",
     "1"  => "b",
@@ -40,5 +46,6 @@ $array2 = array(
     true => "d",
 );
 var_dump($array2);
+
 
 ?>

ef4915dec331d98b823579a5069197991053b2cf
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-24 10:42:10 +0300

    Modified breaks

--- phpfolder/objects.php
+++ phpfolder/objects.php
@@ -1,6 +1,22 @@
 <?php
 //Objects in php
 
+class Game{
+  var $name;
+  var $price;
+  var $photo;
 
+  public function print_Details() {
+    echo "Game: " . $this->name . "<br>\n";
+    echo "Price: $" . $this->price . "<br>\n";
+    echo "Photo: " . $this->photo . "<br>\n";
+  }
 
+}
+$game = new Game;
+$game->name = "Bioshock Infinite";
+$game -> price = 14.99;
+$game -> photo = "bioshock-infinite.jpg";
+
+$game ->print_Details();
 ?>

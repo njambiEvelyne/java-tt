8d5afd9946ebc1723ad04c8fb440072a2c8285a4
njambiEvelyne <evelynenganga76@gmail.com>
2025-12-26 08:32:24 +0300

    Added a new file

---
+++ phpfolder/switch.php
@@ -0,0 +1,14 @@
+<?php
+$favcolor = "red";switch ($favcolor){
+  case "red":
+    echo "Your favourite color is red";
+    break;
+  
+  case "green":
+    echo "Your favourite color is green";
+    break;
+
+  case blu
+}
+
+?>
